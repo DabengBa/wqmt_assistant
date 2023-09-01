fd19a0f580355b2c8efc72baf96ec8370193b257
walklty <walklty@hotmail.com>
2023-09-01 17:14:39 +0800

    Learning Class

--- debug.py
+++ debug.py
@@ -1,5 +1,6 @@
-import random
 import subprocess
+import numpy as np
+
 import time
 import cv2
 import yaml
@@ -8,135 +9,161 @@ from pywebio.input import *
 from pywebio.output import *
 import datetime
 
-with open('config.yaml', 'r') as f:
-  config = yaml.safe_load(f)
-remote_path = config['remote_path']
-local_path = config['local_path']
-devicename = config['devicename']
-adb_path = config['adb_path']
+try:
+    with open('config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+except FileNotFoundError:
+    print("Error: config.yaml file not found.")
+
+remote_path = config.get('remote_path', '')
+local_path = config.get('local_path', '')
+devicename = config.get('devicename', '')
+adb_path = config.get('adb_path', '')
+SleepTime = config.get('SleepTime', '')
+
 
 def test_menu():
-  options = ['1', '2']
-  selected_options = actions("嗯……", options)
-  if "1" in selected_options:
-    morning()
-  if "2" in selected_options:
-    night()
-  test_menu() # 递归调用主菜单函数，以实现循环显示菜单
-
-def gen_ran():
-    """
-    des:
-        随机生成一个浮点数 
-    使用方法：
-        ran = gen_ran()
-    """
-    ran = random.randint(-5, 5)
-    return ran
-
-def get_time():
-  """
-  des:
-      获取当前时间，例如 "2023-08-30 08:03:02"
-  使用方法：
-      put_text("开始"，get_time()）
-  """
-  time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-  return time_stamp
+    options = ['1', '2']
+    selected_options = actions("嗯……", options)
+    if "1" in selected_options:
+        morning()
+    if "2" in selected_options:
+        night()
+    test_menu()  # 递归调用主菜单函数，以实现循环显示菜单
 
 # ADB
-## Connection
-def adb_disconnect(): # 断开设备
-  subprocess.run([adb_path, 'disconnect', devicename], 
-                 stdout=subprocess.DEVNULL, 
-                 stderr=subprocess.DEVNULL)
+# Connection
 
-def adb_connect(): # 连接设备，失败则报错
-    result = subprocess.run([adb_path,'connect',devicename], 
-                stdout=subprocess.PIPE, 
-                stderr=subprocess.PIPE)
+
+def adb_disconnect():  # 断开设备
+    subprocess.run([adb_path, 'disconnect', devicename],
+                   stdout=subprocess.DEVNULL,
+                   stderr=subprocess.DEVNULL)
+
+
+def adb_connect():  # 连接设备，失败则报错
+    result = subprocess.run([adb_path, 'connect', devicename],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
     if str(result.stdout).find('cannot') != -1:
         put_text("连接模拟器失败，请见检查congfig中devicename的配置")
 
-## UI Control
-def adb_get_resolution():
-    process = subprocess.Popen([adb_path, '-s', devicename, 'shell', 'wm', 'size'], stdout=subprocess.PIPE)
-    output = process.stdout.read().decode()
-    height, width = map(int, output.split()[-1].split('x'))
-    return width, height
-
-def adb_swipe(x, y, xx, yy, ran=0, sleepn=0.2):
-    if ran == 1:
-        ran = gen_ran()
-        swipe_coordinates = [str(x+ran), str(y+ran), str(xx+ran), str(yy+ran)]
-    else:
-        swipe_coordinates = [str(x), str(y), str(xx), str(yy)]
-    subprocess.run(["adb", "-s", devicename, 
-                    "shell", "input", "touchscreen", "swipe"] + swipe_coordinates)
-    put_text(f"滑动坐标{swipe_coordinates}，{get_time()}")
-    time.sleep(sleepn)
-
-def adb_swipe_percent(x_percent, y_percent, xx_percent, yy_percent, ran=0, sleepn=0.2):
-    x_pixel = round(adb_get_resolution()[0] * x_percent,2)
-    y_pixel = round(adb_get_resolution()[1] * y_percent,2)
-    xx_pixel = round(adb_get_resolution()[0] * xx_percent,2)
-    yy_pixel = round(adb_get_resolution()[1] * yy_percent,2)
-    adb_swipe(x_pixel, y_pixel, xx_pixel, yy_pixel, ran, sleepn)
-
-def adb_click(x, y,ran=0, sleepn=0.2):
-    if ran == 1:
-        ran = gen_ran()
-        click_coordinates = [str(x+ran), str(y+ran)]
-    else:
-        click_coordinates = [str(x), str(y)]
-    subprocess.run([adb_path, "-s", devicename, "shell", "input", "tap"] + click_coordinates)
-    put_text(f"点击坐标{click_coordinates}，{get_time()}")
-    time.sleep(sleepn)
-  
-def adb_click_percent(x_percent, y_percent,ran=0, sleepn=0.2):
-    x_pixel = round(adb_get_resolution()[0] * x_percent,2)
-    y_pixel = round(adb_get_resolution()[1] * y_percent,2)
-    adb_click(x_pixel, y_pixel, ran, sleepn)
-
-def adb_screenshot():# 屏幕截图覆盖screenshop.png, 使用DEVNULL避免输出命令行
-  subprocess.run([adb_path, '-s', devicename, 'shell', 'screencap', remote_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-  subprocess.run([adb_path, '-s', devicename, 'pull', remote_path, local_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-  put_image(open(local_path, 'rb').read(),width='500px')
-
-#CV
-def comparebackxy(targetpic,threshold=0.9): #找图，返回坐标
-  adb_screenshot()
-  img = cv2.imread(local_path, 0) # 屏幕图片
-  template = cv2.imread(targetpic, 0) # 寻找目标
-  h, w = template.shape[:2]
-  res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)# 相关系数匹配方法：cv2.TM_CCOEFF
-  _, max_val, _, max_loc = cv2.minMaxLoc(res)
-  x, y = max_loc[0] + w // 2, max_loc[1] + h // 2
-  put_text(f"寻找{targetpic}, 最大匹配度{max_val:.2f}，坐标{x},{y}")
-  return (x, y) if max_val > threshold else None
-
-def test_comparebackxy(targetpic,threshold=0.9):
-  adb_screenshot()
-  img = cv2.imread(local_path, 0)
-  template = cv2.imread(targetpic, 0)  
-  h, w = template.shape[:2]
-  # 限定搜索区域
-  roi_w = img.shape[1] // 2 
-  roi = img[:,roi_w:]
-  # 模板匹配
-  res = cv2.matchTemplate(roi, template, cv2.TM_CCOEFF_NORMED)
-  # 其余逻辑不变
-  _, max_val, _, max_loc = cv2.minMaxLoc(res) 
-  x, y = max_loc[0] + roi_w + w//2, max_loc[1] + h//2
-  return (x, y) if max_val > threshold else None
+# UI Control
 
 
+class RandomCoords():
+    def __init__(self):
+        pass
 
-def compare_click(targetpic, threshold=0.9, sleepn=0.2, times=1, success="success",fail="fail"):
-    center = comparebackxy(targetpic,threshold)
+    def xy(self, x, y, xx=0, yy=0):
+        while True:
+            mx = np.round(np.random.choice(np.random.normal(
+                loc=x, scale=7, size=15)), decimals=2)
+            my = np.round(np.random.choice(np.random.normal(
+                loc=y, scale=7, size=15)), decimals=2)
+            mxx = np.round(np.random.choice(np.random.normal(
+                loc=xx, scale=7, size=15)), decimals=2)
+            myy = np.round(np.random.choice(np.random.normal(
+                loc=yy, scale=7, size=15)), decimals=2)
+            if x-15 < mx < x+15 and y-15 < my < y+15 and xx-15 < mxx < xx+15 and yy-15 < myy < yy+15:
+                return mx, my, mxx, myy
+
+    def time(self, SleepTime):
+        while True:
+            mtime = np.round(np.random.choice(np.random.normal(
+                loc=SleepTime, scale=SleepTime*0.3, size=15)), decimals=2)
+
+            if SleepTime < mtime < SleepTime*1.3:
+                random_float = np.random.random()
+                if random_float > 0.8:
+                    mtime = mtime+1
+                return mtime
+
+
+def get_time():
+    """
+    des:
+        获取当前时间，例如 "2023-08-30 08:03:02"
+    使用方法：
+        put_text("开始"，get_time()）
+    """
+    time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return time_stamp
+
+
+class ScreenCtrl():
+    def __init__(self):
+        pass
+
+    def adb_get_resolution(self):
+        process = subprocess.Popen([adb_path, '-s', devicename, 'shell', 'wm', 'size'],
+                                   stdout=subprocess.PIPE)
+        output = process.stdout.read().decode()
+        height, width = map(int, output.split()[-1].split('x'))
+        return width, height
+
+    def swipe(self, x, y, xx, yy, SleepTime=SleepTime):
+        if x < 1:
+            x, y, xx, yy = self.percent(x, y, xx, yy)
+        x, y, xx, yy = RandomCoords().xy(x, y, xx, yy)
+        swipe_coords = list(map(str, [x, y, xx, yy]))
+        time_gap = RandomCoords().time(SleepTime)
+        subprocess.run(["adb", "-s", devicename,
+                        "shell", "input", "touchscreen", "swipe"] + swipe_coords)
+        put_text(f"滑动坐标{swipe_coords}，将休息{time_gap}秒，{get_time()}")
+        time.sleep(time_gap)
+
+    def click(self, x, y, SleepTime=SleepTime):
+        if x < 1:
+            Codi = self.percent(x, y)
+            x = Codi[0]
+            y = Codi[1]
+        Codi = RandomCoords().xy(x, y)
+        x = Codi[0]
+        y = Codi[1]
+        click_coords = list(map(str, [x, y]))
+        time_gap = RandomCoords().time(SleepTime)
+        subprocess.run([adb_path, "-s", devicename, "shell",
+                       "input", "tap"] + click_coords)
+        put_text(f"点击坐标{click_coords}，将休息{time_gap}秒，{get_time()}")
+        time.sleep(time_gap)
+
+    def percent(self, x, y, xx=0, yy=0):
+        x = round(self.adb_get_resolution()[0] * x, 2)
+        y = round(self.adb_get_resolution()[1] * y, 2)
+        xx = round(self.adb_get_resolution()[0] * xx, 2)
+        yy = round(self.adb_get_resolution()[1] * yy, 2)
+        return x, y, xx, yy
+
+
+def adb_screenshot():  # 屏幕截图覆盖screenshop.png, 使用DEVNULL避免输出命令行
+    subprocess.run([adb_path, '-s', devicename, 'shell', 'screencap',
+                   remote_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run([adb_path, '-s', devicename, 'pull', remote_path,
+                   local_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    put_image(open(local_path, 'rb').read(), width='500px')
+
+# CV
+
+
+def comparebackxy(targetpic, threshold=0.9):  # 找图，返回坐标
+    adb_screenshot()
+    img = cv2.imread(local_path, 0)  # 屏幕图片
+    template = cv2.imread(targetpic, 0)  # 寻找目标
+    h, w = template.shape[:2]
+    # 相关系数匹配方法：cv2.TM_CCOEFF
+    res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+    _, max_val, _, max_loc = cv2.minMaxLoc(res)
+    x, y = max_loc[0] + w // 2, max_loc[1] + h // 2
+    put_text(f"寻找{targetpic}, 最大匹配度{max_val:.2f}，坐标{x},{y}")
+    return (x, y) if max_val > threshold else None
+
+
+def compare_click(targetpic, threshold=0.9, sleepn=0.2, times=1, success="success", fail="fail"):
+    center = comparebackxy(targetpic, threshold)
     if center:
-        put_text(success,get_time())
-        x,y = center
+        put_text(success, get_time())
+        x, y = center
         for _ in range(times):
             adb_click(x, y)
             time.sleep(sleepn)
--- test.py
+++ test.py
@@ -1,21 +1,106 @@
-from debug import *
-from wqmt import *
+# from debug import *
+# from wqmt import *
 from pywebio.input import *
 from pywebio.output import *
 from pywebio import *
+import numpy as np
+import yaml
+import subprocess
+import datetime
+import time
+
 # import uiautomator2 as u2 # 在家获取activities信息
 
-""" with open('config.yaml', 'r') as f:
-  config = yaml.safe_load(f)
-remote_path = config['remote_path']
-local_path = config['local_path']
-devicename = config['devicename']
-adb_path = config['adb_path']
- """
+try:
+    with open('config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+except FileNotFoundError:
+    print("Error: config.yaml file not found.")
 
-adb_connect()
-adb_click(201,374)
+remote_path = config.get('remote_path', '')
+local_path = config.get('local_path', '')
+devicename = config.get('devicename', '')
+adb_path = config.get('adb_path', '')
+SleepTime = config.get('SleepTime', '')
 
-# 切片
-# adb_click(201,374)
-# adb_swipe_percent(0.1,0.1,0.2,0.2)
+class RandomCoords():
+  def __init__(self):
+    pass
+
+  def xy(self, x, y, xx=0, yy=0):
+    while True:
+      mx = np.round(np.random.choice(np.random.normal(loc=x, scale=7, size=15)),decimals=2)
+      my = np.round(np.random.choice(np.random.normal(loc=y, scale=7, size=15)),decimals=2)
+      mxx = np.round(np.random.choice(np.random.normal(loc=xx, scale=7, size=15)),decimals=2)
+      myy = np.round(np.random.choice(np.random.normal(loc=yy, scale=7, size=15)),decimals=2)
+      if x-15 < mx < x+15 and y-15 < my < y+15 and xx-15 < mxx < xx+15 and yy-15 < myy < yy+15:
+        return mx, my, mxx, myy
+  
+  def time(self, SleepTime):
+    while True:
+      mtime = np.round(np.random.choice(np.random.normal(loc=SleepTime, scale=SleepTime*0.3, size=15)),decimals=2)
+
+      if SleepTime < mtime < SleepTime*1.3:
+        random_float = np.random.random()
+        if random_float > 0.8:
+           mtime = mtime+1
+        return mtime
+
+def get_time():
+  """
+  des:
+      获取当前时间，例如 "2023-08-30 08:03:02"
+  使用方法：
+      put_text("开始"，get_time()）
+  """
+  time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  return time_stamp
+
+class ScreenCtrl():
+    def __init__(self):
+        pass
+        
+    def adb_get_resolution(self):
+        process = subprocess.Popen([adb_path, '-s', devicename, 'shell', 'wm', 'size'], 
+                                   stdout=subprocess.PIPE)
+        output = process.stdout.read().decode()
+        height, width = map(int, output.split()[-1].split('x'))
+        return width, height
+
+    def swipe(self, x, y, xx, yy, SleepTime=SleepTime):
+        if x < 1:
+            x, y, xx, yy = self.percent(x, y, xx, yy)
+        x, y, xx, yy = RandomCoords().xy(x, y, xx, yy)
+        swipe_coords = list(map(str,[x, y, xx, yy]))
+        time_gap = RandomCoords().time(SleepTime)
+        subprocess.run(["adb", "-s", devicename, 
+                        "shell", "input", "touchscreen", "swipe"] + swipe_coords)
+        put_text(f"滑动坐标{swipe_coords}，将休息{time_gap}秒，{get_time()}")
+        time.sleep(time_gap)
+
+    def click(self, x, y, SleepTime=SleepTime):
+        if x < 1:
+            Codi = self.percent(x, y)
+            x = Codi[0]
+            y = Codi[1]
+        Codi = RandomCoords().xy(x, y)
+        x = Codi[0]
+        y = Codi[1]
+        click_coords = list(map(str,[x, y]))
+        time_gap = RandomCoords().time(SleepTime)
+        subprocess.run([adb_path, "-s", devicename, "shell", "input", "tap"] + click_coords)
+        put_text(f"点击坐标{click_coords}，将休息{time_gap}秒，{get_time()}")
+        time.sleep(time_gap)
+
+    def percent(self, x, y, xx=0, yy=0):
+        x = round(self.adb_get_resolution()[0] * x,2)
+        y = round(self.adb_get_resolution()[1] * y,2)
+        xx = round(self.adb_get_resolution()[0] * xx,2)
+        yy = round(self.adb_get_resolution()[1] * yy,2)
+        return x, y, xx, yy
+
+subprocess.run([adb_path,'connect',devicename], 
+                stdout=subprocess.PIPE, 
+                stderr=subprocess.PIPE)
+
+ScreenCtrl().click(0.2,0.5,SleepTime=5)
