2bbfd018618093e11b5b4a75fdb2bbe1a5d6687b
YUASDS <1787569211@qq.com>
2023-11-21 18:47:47 +0800

    增加B服适配，增加体力挑战关卡，增加结束后退出游戏，修正监察密令

--- main.py
+++ main.py
@@ -6,47 +6,48 @@ import utils.adb as adb
 
 if __name__ == "__main__":
     adb.connect()
-    pw.output.put_text("请提前在Config.yaml中配置好mumu的ip地址和端口")
     pw.output.put_text("建议按照12小时间隔，早晚各一次。晚上执行的时候请在17点之后，以便领取体力")
-
-    options = ["早一次", "晚一次", "自选", "单刷肉鸽"]
-    selected_options = pw.input.actions("嗯……", options)
+    last = ["无", "退出模拟器", "退出无期迷途"]
+    selected_last = pw.input.select("结束选择", last)  # type: ignore
+    # pw.input.
     if cfg.log_switch == "close":
-        pw.output.put_text(f"config.yaml中已经关闭日志输出，之后本窗口可以关闭")
-    if "早一次" in selected_options:
-        wq.morning()
-        log.logit(f"完成所有任务").text()
-    if "晚一次" in selected_options:
-        wq.night()
-        log.logit(f"完成所有任务").text()
-    if "自选" in selected_options:
-        log.logit(f"打开自选界面").text()
-        agree = wq.select_jobs()
-        if "启动" in agree:
-            wq.starttohome()
-        if "签到" in agree:
-            wq.dailycheckin()
-        if "公会" in agree:
-            wq.guild()
-        if "邮件" in agree:
-            wq.getmail()
-        if "采购中心-每日免费体力" in agree:
-            wq.purchase()
-        if "基建收菜" in agree:
-            wq.construction()
-        if "管理局" in agree:
-            wq.Bureau()
-        if "好友" in agree:
-            wq.friends()
-        if "监察密令" in agree:
-            wq.supervision()
-        if "副本-锈河记忆" in agree:
-            wq.raidriver()
-        if "副本-11-6" in agree:
-            wq.raid11()
-        if "副本-深井" in agree:
-            wq.raiddark()
-        log.logit("完成所有任务").text()
-    if "单刷肉鸽" in selected_options:
+        pw.output.put_text("config.yaml中已经关闭日志输出，之后本窗口可以关闭")
+    agree = wq.select_jobs()  # type: ignore
+
+    fight_options = ["无", "狄斯币", "狂厄结晶", "副本-11-6"]
+    fight = pw.input.select("Fight", fight_options, value=["狄斯币"])  # type: ignore
+    if "启动" in agree:
         wq.starttohome()
+    if "签到" in agree:
+        wq.dailycheckin()
+    if "公会" in agree:
+        wq.guild()
+    if "邮件" in agree:
+        wq.getmail()
+    if "采购中心-每日免费体力" in agree:
+        wq.purchase()
+    if "基建收菜" in agree:
+        wq.construction()
+    if "管理局" in agree:
+        wq.Bureau()
+    if "好友" in agree:
+        wq.friends()
+    if "副本-锈河记忆" in agree:
+        wq.raidriver("storm")
+    if "副本-深井" in agree:
+        wq.raiddark()
+    if "无" not in fight:
+        if "副本-11-6" in fight:
+            wq.raid11()
+        if "狄斯币" in fight:
+            wq.raidriver("gold")
+        if "狂厄结晶" in fight:
+            wq.raidriver("crystal")
+    if "监察密令" in agree:
+        wq.supervision()
+    if "肉鸽" in agree:
         wq.rouge()
+    log.logit("完成所有任务").text()
+    if "退出无期迷途" in selected_last:
+        wq.wqmtclose()
+        log.logit("退出完成").text()
--- utils/config.py
+++ utils/config.py
@@ -6,7 +6,7 @@ from sys import argv
 # 动态参数
 ## 常用参数
 curr_dir = path.join(path.dirname(argv[0]), "")
-default_threshold = float(0.85)
+default_threshold = 0.85
 ## 环境参数
 ocr_dir = path.join(curr_dir, "tools", "PaddleOCR-json", "PaddleOCR-json.exe")
 adb_dir = path.join(curr_dir, "tools", "adb.exe")
@@ -16,17 +16,15 @@ formatted_today = time.strftime("%Y-%m-%d", time.localtime(time.time()))
 #! 以上参数在引用时需要使用类似config.ocr_dir的格式
 
 # 从Config取值，当前程序专用
-try:
-    with open(path.join(curr_dir, "config.yaml"), "r", encoding="utf-8") as f:
-        yaml = YAML()
-        config = yaml.load(f)
-except FileNotFoundError:
-    print("Error: config.yaml file not found.")
+with open(path.join(curr_dir, "config.yaml"), "r", encoding="utf-8") as f:
+    yaml = YAML()
+    config = yaml.load(f)
 
 log_switch = config.get("log_switch", "")
 sleep_time = config.get("sleep_time", "")
 remote_dir = config.get("remote_dir", "")
 device_name = config.get("device_name", "")
+sever_type = config.get("sever_type", "")
 width, height = (
     map(int, config.get("dimensions", "").split(","))
     if config.get("dimensions")
